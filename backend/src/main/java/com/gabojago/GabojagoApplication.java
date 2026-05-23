425aed4d4a0f6b903daf28736917001e083010cc
YeonWoo <kobin4215@naver.com>
2026-05-24 01:46:12 +0900

    refactor: 인증 및 사용자 도메인 구조 정리

--- backend/src/main/java/com/gabojago/GabojagoApplication.java
+++ backend/src/main/java/com/gabojago/GabojagoApplication.java
@@ -1,4 +1,4 @@
-package com.tripmate.backend;
+package com.gabojago;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
