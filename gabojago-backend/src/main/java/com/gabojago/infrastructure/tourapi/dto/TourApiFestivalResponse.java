1c85b4c917a41286c73fb5e05dfb98b523327097
YeonWoo <kobin4215@naver.com>
2026-08-08 14:54:32 +0900

    refactor: 혼잡도 구현 제거 및 재개 조건 문서화

--- gabojago-backend/src/main/java/com/gabojago/infrastructure/tourapi/dto/TourApiFestivalResponse.java
+++ gabojago-backend/src/main/java/com/gabojago/infrastructure/tourapi/dto/TourApiFestivalResponse.java
@@ -1,52 +1,52 @@
 package com.gabojago.infrastructure.tourapi.dto;
-import com.fasterxml.jackson.annotation.JsonProperty;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+
 import java.util.List;
+
+/** locationBasedList2 응답 중 축제 조회에 필요한 필드만 매핑한다. */
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TourApiResponse {
+public class TourApiFestivalResponse {
+
     private Response response;
+
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Response {
         private Header header;
         private Body body;
     }
+
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Header {
         private String resultCode;
         private String resultMsg;
     }
+
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Body {
         private Items items;
-        private int numOfRows;
-        private int pageNo;
         private int totalCount;
     }
+
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Items {
         private List<Item> item;
     }
+
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Item {
         private String contentid;
         private String title;
         private String addr1;
-        private String firstimage;
         private double mapx;
         private double mapy;
-        private String areacode;
-        private String sigungucode;
-        @JsonProperty("lDongRegnCd")
-        private String lDongRegnCd;
-        @JsonProperty("lDongSignguCd")
-        private String lDongSignguCd;
         private String eventstartdate;
         private String eventenddate;
     }
--- gabojago-backend/src/main/java/com/gabojago/tourism/data/repository/CongestionApiItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gabojago.tourism.data.repository;
-
-import com.gabojago.tourism.data.domain.CongestionApiItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CongestionApiItemRepository extends JpaRepository<CongestionApiItem, Long> {
-}
